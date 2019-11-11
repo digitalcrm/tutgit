392f48672ce1de0607e5df542f7aa1295a4d6abf
digitalcrm <info@digitalcrm.com>
2019-11-11 18:06:16 +0530

    11-11-2019-18-06

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 echo "<h1>Tut Git</h1>";
 echo "<br>";
 echo "<h3>This is git tutorial</h3>";
+echo "<br>";
+echo "<h3>New file created....Test.php</h3>";
---
+++ test.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h3>This is Testing</h3>";
